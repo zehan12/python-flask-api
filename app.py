3e9bf625ca4f1832f0eb4950582305ad4546dd49
Zehan Khan <zkhan@hootboard.com>
2023-11-30 00:38:02 +0530

    feat: :sparkles: add template rendering in flask

--- app.py
+++ app.py
@@ -1,14 +1,20 @@
 # import
-from flask import Flask
+from flask import Flask, render_template
 
 # Create a Flask app
 app = Flask(__name__)
+
 
 # Define route
 # Create a controller
 @app.route("/")
 def run():
     return "{\"message\":\"Hello World Python v1\"}"
+
+@app.route('/user/<name>')
+def hello(name=None):
+  #name=None ensures the code runs even when no name is provided
+  return render_template('./user-profile.html', name=name)
 
 # Driver function
 if __name__ == "__main__":
